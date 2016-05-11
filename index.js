e13ff4c5df9923cc6ab5d3a475bc382139fb2a9d
Taras Mankovski <tarasm@gmail.com>
2016-05-11 11:27:45 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,71 @@
+"use strict";
+
+let gcloud = require('gcloud');
+
+let storage = gcloud.storage();
+
+const DEFAULT_POLL_TIME = 3 * 1000;
+
+class GCSNotifier {
+  constructor(options) {
+    this.ui = options.ui;
+    this.bucket = options.bucket;
+    this.key = options.key;
+    this.pollTime = options.poll || DEFAULT_POLL_TIME;
+  }
+
+  subscribe(notify) {
+    this.notify = notify;
+
+    return this.getCurrentLastModified()
+      .then(() => this.schedulePoll());
+  }
+
+  getLastModified() {
+      let file = storage.bucket(this.bucket).file(this.key);
+      
+      return new Promise(function(resolve, reject){
+        file.get(function(err, file, apiResponse){
+          if (err) {
+            reject(err);
+          }
+          resolve(apiResponse.updated);
+        });
+      });
+  }
+  
+  getCurrentLastModified() {
+    return this.getLastModified()
+      .then(LastModified => {
+        this.lastModified = LastModified;
+      })
+      .catch(() => {
+        this.ui.writeError('error fetching gcloud Storage last modified; notifications disabled');
+      });
+  }
+
+  schedulePoll() {
+    setTimeout(() => {
+      this.poll();
+    }, this.pollTime);
+  }
+
+  poll() {
+    this.getLastModified()
+      .then(LastModified => {
+        this.compareLastModifieds(LastModified);
+        this.schedulePoll();
+      });
+  }
+
+  compareLastModifieds(newLastModified) {
+    if (newLastModified !== this.lastModified) {
+      this.ui.writeLine('config modified; old=%s; new=%s', this.lastModified, newLastModified);
+      this.lastModified = newLastModified;
+      this.notify();
+    }
+  }
+}
+
+
+module.exports = GCSNotifier;
